d9961c08c4835a276ee213120841db734ec75896
Massa Elise <elise.massa.perso@gmail.com>
2022-04-27 13:30:38 +0200

    fonction dessin qui marche mais fais nimp

--- src/main.cpp
+++ src/main.cpp
@@ -1,73 +1,189 @@
 #include <SDL2/SDL.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
 #include <stdlib.h>
 #include <stdio.h>
 
-//include les nouveaux fichiers!!
-#include "game.h"
 #include "square.h"
 
+/* Dimensions initiales et titre de la fenetre */
+static const unsigned int WINDOW_WIDTH = 800;
+static const unsigned int WINDOW_HEIGHT = 600;
+static const char WINDOW_TITLE[] = "Square Dash";
+
+/* Espace fenetre virtuelle */
+static const float GL_VIEW_SIZE = 2.;
+
+/* Nombre minimal de millisecondes separant le rendu de deux images */
+static const Uint32 FRAMERATE_MILLISECONDS = 1000 / 60;
 
 Square* newSquare = nullptr;
 
-static const unsigned int WINDOW_WIDTH = 800;
-static const unsigned int WINDOW_HEIGHT = 800;
-//static const unsigned int BIT_PER_PIXEL = 32;
+static float aspectRatio;
 
-int main(int argc, char** argv)
+void onWindowResized(unsigned int width, unsigned int height)
+{ 
+    aspectRatio = width / (float) height;
+
+    glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    if( aspectRatio > 1) 
+    {
+        gluOrtho2D(
+        -GL_VIEW_SIZE / 2. * aspectRatio, GL_VIEW_SIZE / 2. * aspectRatio, 
+        -GL_VIEW_SIZE / 2., GL_VIEW_SIZE / 2.);
+    }
+    else
+    {
+        gluOrtho2D(
+        -GL_VIEW_SIZE / 2., GL_VIEW_SIZE / 2.,
+        -GL_VIEW_SIZE / 2. / aspectRatio, GL_VIEW_SIZE / 2. / aspectRatio);
+    }
+}
+
+int main(int argc, char** argv) 
 {
-    // Initialisation de la SDL
-    if(-1 == SDL_Init(SDL_INIT_VIDEO)) {
-        fprintf(stderr, "Impossible d'initialiser la SDL. Fin du programme.\n");
+    /* Initialisation de la SDL */
+
+    if(SDL_Init(SDL_INIT_VIDEO) < 0) 
+    {
+        const char* error = SDL_GetError();
+        fprintf(
+            stderr, 
+            "Erreur lors de l'intialisation de la SDL : %s\n", error);
+
+        SDL_Quit();
         return EXIT_FAILURE;
     }
+	
+    /* Ouverture d'une fenetre et creation d'un contexte OpenGL */
 
-    // Creation de la fenetre SDL
-    SDL_Window* window = SDL_CreateWindow("Square_dash", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, 0);
-    if(window == NULL) {
-        fprintf(stderr, "Impossible de créer la fenêtre. Fin du programme.\n");
-        return EXIT_FAILURE;
+    SDL_Window* window;
+    {
+        window = SDL_CreateWindow(
+        WINDOW_TITLE,
+        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+        WINDOW_WIDTH, WINDOW_HEIGHT,
+        SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+
+        if(NULL == window) 
+        {
+            const char* error = SDL_GetError();
+            fprintf(
+                stderr,
+                "Erreur lors de la creation de la fenetre : %s\n", error);
+
+            SDL_Quit();
+            return EXIT_FAILURE;
+        }
     }
-
-    SDL_Surface* surface = SDL_GetWindowSurface(window);
-    if(surface == NULL) {
-        fprintf(stderr, "Impossible d'ouvrir la fenetre. Fin du programme.\n");
-        return EXIT_FAILURE;
-    }
-
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
-    if(renderer == NULL) {
-        fprintf(stderr, "Impossible de créer le renderer. Fin du programme.\n");
-        return EXIT_FAILURE;
-    }
-
-    // Fonctions pour jouer
-    printf("%d", test());
-
-    //test d'afficher un square
-
-    *newSquare = Square(1, 10., 10., 10., 10., 1., 0., 0.);
     
-    newSquare->drawSquare(0);
+    SDL_GLContext context;
+    {
+            SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+            SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
 
+            SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
+        context = SDL_GL_CreateContext(window);
+    
+        if(NULL == context) 
+        {
+            const char* error = SDL_GetError();
+            fprintf(
+                stderr,
+                "Erreur lors de la creation du contexte OpenGL : %s\n", error);
+
+            SDL_DestroyWindow(window);
+            SDL_Quit();
+            return EXIT_FAILURE;
+        }
+    }    
+  
+    onWindowResized(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+  
+    /* Boucle principale */
     int loop = 1;
-    while(loop) {
+    while(loop) 
+    {
+        /* Recuperation du temps au debut de la boucle */
+        Uint32 startTime = SDL_GetTicks();
+        
+        /* Placer ici le code de dessin */
+        glClear(GL_COLOR_BUFFER_BIT);
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
 
-        SDL_RenderClear(renderer);
-        //SDL_RenderCopy(renderer, texture, NULL, NULL);
-        SDL_RenderPresent(renderer);
-
+        Square newSquare(1, 10., 10., 10., 10., 1., 0., 0.);
+    
+        newSquare.drawSquare(1);
+        
+        /* Echange du front et du back buffer : mise a jour de la fenetre */
+        SDL_GL_SwapWindow(window);
+        
+        /* Boucle traitant les evenements */
         SDL_Event e;
-        while(SDL_PollEvent(&e)) {
-            if(e.type == SDL_QUIT) {
-                loop = 0;
-                break;
+        while(SDL_PollEvent(&e)) 
+        {
+            /* L'utilisateur ferme la fenetre : */
+			if(e.type == SDL_QUIT) 
+			{
+				loop = 0;
+				break;
+			}
+		
+			if(	e.type == SDL_KEYDOWN 
+				&& (e.key.keysym.sym == SDLK_q || e.key.keysym.sym == SDLK_ESCAPE))
+			{
+				loop = 0; 
+				break;
+			}
+            
+            switch(e.type) 
+            {
+                case SDL_WINDOWEVENT:
+                    switch (e.window.event) 
+                    {
+                        /* Redimensionnement fenetre */
+                        case SDL_WINDOWEVENT_RESIZED:
+                            onWindowResized(e.window.data1, e.window.data2);                
+                            break;
+
+                        default:
+                            break; 
+                    }
+                    break;
+
+                /* Clic souris */
+                case SDL_MOUSEBUTTONUP:
+                    printf("clic en (%d, %d)\n", e.button.x, e.button.y);
+                    break;
+                
+                /* Touche clavier */
+                case SDL_KEYDOWN:
+                    printf("touche pressee (code = %d)\n", e.key.keysym.sym);
+                    break;
+                    
+                default:
+                    break;
             }
+        }
+
+        /* Calcul du temps ecoule */
+        Uint32 elapsedTime = SDL_GetTicks() - startTime;
+        /* Si trop peu de temps s'est ecoule, on met en pause le programme */
+        if(elapsedTime < FRAMERATE_MILLISECONDS) 
+        {
+            SDL_Delay(FRAMERATE_MILLISECONDS - elapsedTime);
         }
     }
 
+    /* Liberation des ressources associees a la SDL */ 
+    SDL_GL_DeleteContext(context);
+    SDL_DestroyWindow(window);
     SDL_Quit();
-
+    
     return EXIT_SUCCESS;
 }
-
